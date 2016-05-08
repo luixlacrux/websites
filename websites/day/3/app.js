76e699222e165d85f8e877498e3b543291c0bbfd
luixlacrux <luixlacrux18@gmail.com>
2016-05-08 15:09:59 -0430

    add day 3

---
+++ websites/day/3/app.js
@@ -0,0 +1,17 @@
+function genRandomColor () {
+  var letters = '0123456789ABCDEF'.split('')
+  var color = '#'
+  for (var i = 0; i < 6; i++ ) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
+
+const colorInit = '#ffa500'
+const buttonChange = document.getElementById('button')
+buttonChange.style.background = colorInit
+
+buttonChange.addEventListener('click', function () {
+  document.body.style.background = this.style.background
+  this.style.background = genRandomColor()
+})
